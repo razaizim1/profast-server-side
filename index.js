8d07f66a8f02caa4409c27d5fd3d7a1388d4f96c
razaizim1 <zim.sarkar5956@gmail.com>
2025-07-04 11:02:42 +0600

    delete mathod added

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jzh2fbg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -53,6 +53,17 @@ async function run() {
                 res.json(result);
             } catch (error) {
                 res.status(500).json({ error: 'Failed to fetch parcels' });
+            }
+        });
+
+        app.delete('/parcels/:id', async (req, res) => {
+            const id = req.params.id;
+            try {
+                const result = await parcels.deleteOne({ _id: new ObjectId(id) });
+                res.json(result);
+            } catch (err) {
+                console.error(err);
+                res.status(500).json({ error: 'Delete failed' });
             }
         });
 
